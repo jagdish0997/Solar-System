fde4122ee738dc77fcf0740a52f86a79a41412fc
jagdish0997 <jagdishready47@gmail.com>
2023-06-01 18:19:27 +0530

    Solar-System

---
+++ src/Component/Solarr.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import earth from '../Image/plant.png';
+
+
+const Solar = () => {
+  return (
+      <div>
+      <body>
+        <div class="container">
+          <div class="sun"></div>
+          <div class="earth">
+            <div class="moon"></div>
+          </div>
+          <div className='mars'>
+          </div>
+        </div>
+      </body>
+    </div>
+  )
+}
+
+export default Solar
